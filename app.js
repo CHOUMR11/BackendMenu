f3799bd6d52de8bedb63dc5b0be10467290b22f6
Your Name <you@example.com>
2025-06-04 21:24:37 +0100

    final to new structure and fix serverless crash

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+
 const connectDB = require('./config/db');
 const menuRoutes = require('./routes/menuRoutes');
 const orderRoutes = require('./routes/orderRoutes');
-const path = require('path');
-const serverless = require('serverless-http');
+
 const app = express();
 
-let dbConnected = false;
-if (!dbConnected) {
-  connectDB();
-  dbConnected = true;
-}
+connectDB();
 
 app.use(cors());
 app.use(express.json());
 
+// Routes API
 app.use('/api/menu', menuRoutes);
 app.use('/api/orders', orderRoutes);
-app.use(express.static(path.join(__dirname, './client/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
 
-module.exports = serverless(app);
+// ❌ REMOVE app.listen() for Vercel!
+module.exports = app;
